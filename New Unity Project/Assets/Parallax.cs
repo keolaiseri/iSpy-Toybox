cd8b7143cfe7089e4652f3e2d18719f2d313165c
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-02-21 16:07:19 -1000

    Code complete

--- New Unity Project/Assets/Parallax.cs
+++ New Unity Project/Assets/Parallax.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+
+
+    private float length, startpos;
+    public GameObject caml;
+    public float parallaxEffect;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+           
+
+
+
+
+
     }
 
+
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float dist = (caml.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+
+
+
     }
 }
