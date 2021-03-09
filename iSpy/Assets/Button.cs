881ff0f7595158d1c493f91549f69ff7f418d212
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-03-08 16:45:55 -1000

    working on animation

---
+++ iSpy/Assets/Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+
+    [SerializeField]
+
+    float time;
+    [SerializeField]
+    public LeanTweenType type;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        LeanTween.moveY(gameObject, 10f, 1f).setEase(type);
+        LeanTween.moveX(gameObject, 10f, 1f).setEase(type);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
